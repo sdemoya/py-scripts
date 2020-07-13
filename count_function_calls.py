ba8bd3f112e6afd7b4c9ecf3cff7710764c8a501
Skye <hello@skyedemoya.com>
2020-07-13 23:09:49 +0000

    Fixed spelling error.

--- count_function_calls.py
+++ count_function_calls.py
@@ -1,7 +1,7 @@
 print("Welcome to my little script. I read that, theoretically, a function can be called in an infinite number of ways. Let's see how many ways I can come up with today.")
 
 def nachoes(salsa):
-    print(f"So you like your nachoes with salsa {salsa}. Sounds delicious!")
+    print(f"So you like your nachos with salsa {salsa}. Sounds delicious!")
 
 print("Let's count how many ways I call this function:")
 
