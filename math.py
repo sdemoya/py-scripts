d6fcc4aab991f6a5d691a6a8e36575820b6d518e
skyedemoya <hello@skyedemoya.com>
2020-08-01 05:40:18 +0000

    More practice.

--- math.py
+++ math.py
@@ -1,6 +1,6 @@
 # Let's try this again. I started this script but didn't save it before walking away and got booted off my virtual server. Whoops!
 
-# This is just another little script for me to practice what I've learned so far. The code is not from an exercise.
+# This is just another little script for me to practice what I've learned so far.
 
 print("An integer is a whole number, and a float is a number with a decimal point.\n The number twenty is displayed as each respectively below.")
 print(int(20))
@@ -19,4 +19,23 @@ print(8 % 3)
 print(8 % 2)
 
 print((5 * 2) ** 2 / 2)
+
+print("You can use underscores to separate numbers and make them more human friendly, without changing the value of the number. It also doesn't matter where you put the underscores.")
+
+big_num = 20_000_000
+big_num2 = 2_00000_00
+
+print(f"{big_num} is the same as {big_num2}")
+
+print("See? They print out as the same number with no underscores.")
+
+print("You can also assign multiple variables with a single line of code, such as, x, y, z = 1, 2, 3.")
+
+x, y, z = 1, 2, 3
+
+print(f"This is x: {x}")
+print(f"This is y: {y}")
+print(f"This is z: {z}")
+
+print(x + y)
 
---
+++ misc.py
@@ -0,0 +1,7 @@
+# This script is for me to pratice things that don't fit into neat little sections.
+
+print("Python does not support constants (variables that stay the same throughout the life of a program). However, programmers use all caps for variable names to indicate that such should be treated as a constant.")
+
+HELLO = "hi"
+
+print(HELLO)
