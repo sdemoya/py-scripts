12b43ddc99c1141faae45243430c983b1978e7cb
skyedemoya <hello@skyedemoya.com>
2020-08-01 05:10:11 +0000

    Created a file to pratice mathematical operations.

---
+++ math.py
@@ -0,0 +1,22 @@
+# Let's try this again. I started this script but didn't save it before walking away and got booted off my virtual server. Whoops!
+
+# This is just another little script for me to practice what I've learned so far. The code is not from an exercise.
+
+print("An integer is a whole number, and a float is a number with a decimal point.\n The number twenty is displayed as each respectively below.")
+print(int(20))
+print( float(20))
+
+print("Python will default to a float with any operation involving a float, even if the result is a whole number.")
+
+print(5 / 2.5)
+
+print(2 ** 4)
+
+print(8 / 3)
+
+print(8 % 3)
+
+print(8 % 2)
+
+print((5 * 2) ** 2 / 2)
+
