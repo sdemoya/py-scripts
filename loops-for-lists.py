4db7b61ea34bd19709dbf853ab3ba0d0f23b6d41
skyedemoya <hello@skyedemoya.com>
2020-08-05 00:20:27 +0000

    Organized files and created new script for for loops.

---
+++ loops-for-lists.py
@@ -0,0 +1,6 @@
+print("We can use for loops to complete an action on each item in a list.")
+
+rosie_attributes = ['fluffy', 'smart', 'loyal', 'sweet', 'calm']
+
+for attribute in rosie_attributes:
+    print("Rosie is ", attribute)
