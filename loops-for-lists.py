428d19476a1f37b2d08a2ac10e01244b7f24fb18
skyedemoya <hello@skyedemoya.com>
2020-08-05 00:25:20 +0000

    Added for loops.

--- loops-for-lists.py
+++ loops-for-lists.py
@@ -3,4 +3,7 @@ print("We can use for loops to complete an action on each item in a list.")
 rosie_attributes = ['fluffy', 'smart', 'loyal', 'sweet', 'calm']
 
 for attribute in rosie_attributes:
-    print("Rosie is ", attribute)
+    print("Rosie is a", attribute, "kitty.")
+
+for rose in rosie_attributes:
+    print(f"{rose.title()} describes Rosie.")
