ad2b5ef2cd7f6f32db95f1154715f051273609b8
skyedemoya <hello@skyedemoya.com>
2020-08-07 23:25:53 +0000

    Refactored range() notes to make them more readable.

--- more-lists.py
+++ more-lists.py
@@ -6,15 +6,20 @@ print("Let's start things off with the range() function.")
 
 print("The range() function takes three arguments: (start, stop, step).")
 
-print("""Actually, it might be easier to start with a list of numbers.
-        Luckily, we can use range() to quickly create it by using the list constructor like so:
-        list(range(1,30))""")
-
 counter = list(range(1, 30))
 
-print(f"counter = ", counter)
+print("""Actually, it might be easier to start with a list of numbers.
+        Luckily, we can easily create a  list using range() wrapped in the list constructor like so:
+        counter = list(range(1, 30))
+        counter = """, counter)
 
 skip_counter = list(range(0, 30, 2))
 
-print(f"We can also add a step like so, list(range(0, 30, 2)), to get \n\t\t", skip_counter)
+print(f"""We can also add a step parameter like so:
+        step_counter = list(range(0, 30, 2))
+        step_counter = """, skip_counter)
+
+print("""\nYou may have noticed that the list stops short of the stop parameter.
+So if I wanted the number 30 included, I would pass 31 as the stop parameter.""")
+
 
