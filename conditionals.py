151ac5513bd445dcdd6c2b6e231b0964e6648875
skyedemoya <hello@skyedemoya.com>
2020-08-13 17:18:28 +0000

    Started a file for conditional statements.

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+#Conditional Statements
+
+foods = ['greens', 'rice', 'potatoes', 'carrots', 'peppers', 'seeds', 'grapes']
+
+for food in foods:
+    if food == 'greens':
+        print("Eat your", food)
+    else:
+        print("Better skip the", food)
+
+food = 'cake'
+
+
+
