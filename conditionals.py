70967077d26c84d54792f027460cbc58913a71e0
skyedemoya <hello@skyedemoya.com>
2020-08-17 13:43:15 +0000

    Added two addtional conditional statements.

--- conditionals.py
+++ conditionals.py
@@ -10,5 +10,17 @@ for food in foods:
 
 food = 'cake'
 
+if food not in foods:
+    print(f"{food.title()} is not on the list of approved foods.")
+else:
+    print("Yes, ",food, " is an approved food.")
 
+food = 'peppers'
+
+if food in foods:
+    print(f"Go on, have some {food}.")
+else:
+    print(f"Only have a small portion of {food}.")
+
+# print(f"This is a test to see how {food}.title() prints.") That will not title the food variable because it is outside of the curly braces.  Instead it prints peppers.title() as if it were plain text.
 
