8093138848dd508ceb527172927c5f8286a6545a
skyedemoya <hello@skyedemoya.com>
2020-08-23 21:59:38 +0000

    Added basic while loop.

---
+++ crash-course-notes/counter.py
@@ -0,0 +1,10 @@
+print("""While loops will run WHILE the condition set is true.
+        For example, we can run a loop that increments a number
+        by one until it is less than or equal to ten.""")
+
+current_number = 1
+
+while current_number <= 10:
+    print(current_number)
+    current_number += 1
+
