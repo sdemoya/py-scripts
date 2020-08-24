3624a7ca0acd5ba95f714d6e4b85f12b9559c90f
skyedemoya <hello@skyedemoya.com>
2020-08-24 15:02:31 +0000

    Used a continue statement to only return multiples of three.

--- visit.py
+++ visit.py
@@ -12,3 +12,12 @@ active = True
     #else:
      #   print(f"{destination.title()} sounds like a wonderful place to visit!")
 
+# The below uses a continue statement to only print multiples of 3,
+# by exiting the loop if that condition is not met.
+
+current_number = 0
+while current_number < 25:
+    current_number += 1
+    if current_number % 3 != 0:
+        continue
+    print(current_number)
