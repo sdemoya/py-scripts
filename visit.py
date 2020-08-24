c38b86af7484efe849793d1a21c395416a8d574b
skyedemoya <hello@skyedemoya.com>
2020-08-24 14:45:45 +0000

    Created and commented out an infinate loop.

---
+++ visit.py
@@ -0,0 +1,14 @@
+prompt = "Where would you like to travel?"
+prompt += "\n(Enter quit to exit the program.)\n"
+
+active = True
+
+#The below creates an infinate loop, press CTRL+C to exit.
+
+#while active:
+ #   destination = input(prompt)
+  #  if destination == 'quit':
+   #     continue
+    #else:
+     #   print(f"{destination.title()} sounds like a wonderful place to visit!")
+
