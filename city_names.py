b39c10fd229a16a189276f5ff9fc6142083b7c25
skyedemoya <hello@skyedemoya.com>
2020-08-25 22:19:14 +0000

    Wrote a simple function and tested to see if it works.

---
+++ city_names.py
@@ -0,0 +1,9 @@
+def city_names(city, country):
+    """Return neatly formated City, Country"""
+    place = {'City': city, 'Country': country}
+    return place
+
+current_local = city_names('minsk', 'belarus')
+print(current_local)
+
+
