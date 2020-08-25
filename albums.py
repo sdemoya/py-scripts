6cc98d72b2789b6fbcf68d1163bc8e0f7098f8c3
skyedemoya <hello@skyedemoya.com>
2020-08-25 23:46:50 +0000

    Added conditional to make songs parameter optional, but got an unexpected output of None.

--- albums.py
+++ albums.py
@@ -1,9 +1,15 @@
 def albums(artist, title, songs=None):
     """Store information about albums in a dictionary."""
-    album = {'Title': title, 'Artist': artist}
-    return album
+    if songs:
+        album = {'Title': title, 'Artist': artist, 'Songs': songs,}
+        return album
+    else:
+        album = {'Title': title, 'Artist': artist,}
 
 OK_Computer = albums('Radiohead', 'OK Computer', 12)
 print(OK_Computer)
 
+print("\n")
 
+Art_Angels = albums('Grimes', 'Art Angels')
+print(Art_Angels)
