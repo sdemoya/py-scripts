1da6c4adb7f1dcd53aa01261cff355f0d945c25a
skyedemoya <hello@skyedemoya.com>
2020-08-25 22:32:33 +0000

    Removed dictionary for an f-string to format the city, country correctly.

--- city_names.py
+++ city_names.py
@@ -1,7 +1,7 @@
 def city_names(city, country):
     """Return neatly formated City, Country"""
-    place = {'City': city, 'Country': country}
-    return place
+    place = f"{city}, {country}"
+    return place.title()
 
 current_local = city_names('minsk', 'belarus')
 print(current_local)
