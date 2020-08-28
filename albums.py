13835c33fb5d05515aa031ede6d448b4d7071a76
skyedemoya <hello@skyedemoya.com>
2020-08-28 02:02:23 +0000

    Work from yesterday that wasn't working. Looking at it today wtih fresh eyes.

--- albums.py
+++ albums.py
@@ -1,16 +1,33 @@
-def albums(artist, title, songs=None):
-    """Store information about albums in a dictionary."""
-    if songs:
-        album = {'Title': title, 'Artist': artist, 'Songs': songs,}
-        return album
-    else:
-        album = {'Title': title, 'Artist': artist,}
-        return album
+#def albums(artist, title, songs=None):
+#   """Store information about albums in a dictionary."""
+#    if songs:
+#        album = {'Title': title, 'Artist': artist, 'Songs': songs,}
+#        return album
+#    else:
+#        album = {'Title': title, 'Artist': artist,}
+#        return album
 
-OK_Computer = albums('Radiohead', 'OK Computer', 12)
-print(OK_Computer)
+#OK_Computer = albums('Radiohead', 'OK Computer', 12)
+#print(OK_Computer)
 
-print("\n")
+#print("\n")
 
-Art_Angels = albums('Grimes', 'Art Angels')
-print(Art_Angels)
+#Art_Angels = albums('Grimes', 'Art Angels')
+#print(Art_Angels)
+
+"""
+active = True
+
+def user_album(title, artist):
+"""    """Creates dictionary to descibe an album with user input."""
+"""    print("Type q at anytime to quit.")
+    title = input("What the name of your favorite album? > ")
+    artist = input("Whose is that by? > ")
+       #  songs = input("Do you know how many songs are on it? > ")
+
+        if title == 'q':
+            active = False
+        else:
+            fav_album = {'Title': title, 'Artist': artist,}
+            print(f"\nHere's your dictionary, {fav_album}\n")
+"""
