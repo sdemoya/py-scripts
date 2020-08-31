89847c98361164c42264a63fd4472217f35063ae
skyedemoya <hello@skyedemoya.com>
2020-08-31 14:03:18 +0000

    Reorganized and created second file to modify albums.py with user input.

---
+++ mini_programs/albums.py
@@ -0,0 +1,16 @@
+def albums(artist, title, songs=None):
+   """Store information about albums in a dictionary."""
+   if songs:
+        album = {'Title': title, 'Artist': artist, 'Songs': songs,}
+        return album
+   else:
+        album = {'Title': title, 'Artist': artist,}
+        return album
+
+OK_Computer = albums('Radiohead', 'OK Computer', 12)
+print(OK_Computer)
+
+print("\n")
+
+Art_Angels = albums('Grimes', 'Art Angels')
+print(Art_Angels)
