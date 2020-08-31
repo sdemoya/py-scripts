9e7ec2ae69fd8bdd54cb8156958dc34029a3e525
skyedemoya <hello@skyedemoya.com>
2020-08-31 12:55:09 +0000

    Started second review file.

---
+++ review2.py
@@ -0,0 +1,13 @@
+# Review / Practice
+
+#CONDITIONAL (IF)  STATEMENTS
+
+# == equality operator(case sensitive)
+
+# != not equal
+
+# or / and check multiple conditions
+
+# is in
+
+# is not in (check if a valuse is not in a list)
