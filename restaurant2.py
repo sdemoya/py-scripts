74f120de6694292399731936298b0287bd0b9b14
skyedemoya <hello@skyedemoya.com>
2020-09-07 14:46:18 +0000

    Added number_served attribute and served() method to class.

--- restaurant2.py
+++ restaurant2.py
@@ -5,10 +5,15 @@ class Restaurant:
     def __init__(self, name, cuisine):
         self.name = name
         self.cuisine = cuisine
+        self.number_served = 0
 
     def describe_restaurant(self):
         """Describe the restaurant."""
         print(f"{self.name} serves {self.cuisine} cuisine.")
+
+    def served(self):
+        """Prints a sentence about patrons served."""
+        print(f"Rosie Cakes has served {self.number_served} patrons today.")
 
 la_vegan  = Restaurant('LA Vegan', 'Asian and American style')
 
@@ -22,4 +27,14 @@ sage = Restaurant('Sage', 'vegan brunch')
 
 sage.describe_restaurant()
 
+my_restaurant = Restaurant('Rosie Cakes', 'baked')
 
+my_restaurant.number_served = 42
+
+my_restaurant.describe_restaurant()
+
+my_restaurant.served()
+
+my_restaurant.number_served = 57
+
+my_restaurant.served()
