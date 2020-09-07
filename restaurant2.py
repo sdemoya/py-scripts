7e4a5db827c3e4769622cf8896fe9e3428f76ef2
skyedemoya <hello@skyedemoya.com>
2020-09-07 00:53:47 +0000

    Copied restaurant.py and moved original into a folder.

---
+++ restaurant2.py
@@ -0,0 +1,25 @@
+
+class Restaurant:
+    """A simple attempt to model a restaurant."""
+
+    def __init__(self, name, cuisine):
+        self.name = name
+        self.cuisine = cuisine
+
+    def describe_restaurant(self):
+        """Describe the restaurant."""
+        print(f"{self.name} serves {self.cuisine} cuisine.")
+
+la_vegan  = Restaurant('LA Vegan', 'Asian and American style')
+
+la_vegan.describe_restaurant()
+
+settabello = Restaurant('Settabello', 'Italian')
+
+settabello.describe_restaurant()
+
+sage = Restaurant('Sage', 'vegan brunch')
+
+sage.describe_restaurant()
+
+
