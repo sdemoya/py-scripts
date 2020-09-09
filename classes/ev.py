c655fcc4fe8ae777ec59e62d08ab2889fcf9e5c4
skyedemoya <hello@skyedemoya.com>
2020-09-09 01:46:04 +0000

    Added more code from Python Crash Course.

--- classes/ev.py
+++ classes/ev.py
@@ -41,6 +41,16 @@ class Battery:
         """Print a statement describing the battery size."""
         print(f"This car has a {self.battery_size}-kWh battery.")
 
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
 
@@ -52,8 +62,8 @@ class ElectricCar(Car):
         super().__init__(make, model, year)
         self.battery = Battery()
 
-my_tesla = ElectricCar('tesla', 'model s', 2019)
 
+my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
-
+my_tesla.battery.get_range()
