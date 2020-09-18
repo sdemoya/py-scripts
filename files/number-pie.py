64b06eb7a60fea95e41e2e2d37b8cf5e6ec01929
skyedemoya <hello@skyedemoya.com>
2020-09-18 03:36:52 +0000

    Brought it all together with remember_number().

--- files/number-pie.py
+++ files/number-pie.py
@@ -11,6 +11,7 @@ def get_stored_number():
     else:
         return num
 
+
 def get_number():
     """Prompt user for their favorite number and store it in a JSON file."""
     num = input("What's your favorite number? ")
@@ -19,7 +20,14 @@ def get_number():
         json.dump(num, f)
     return num
 
-print(get_stored_number())
-print(get_number())
 
+def remember_number():
+    """Return the user's stored favorite number."""
+    num = get_stored_number()
+    if num:
+        print(f"My favorite number is {num} too!")
+    else:
+        num = get_number()
+        print(f"I'll remember your favorite number when you come back!")
 
+remember_number()
