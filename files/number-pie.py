ac9fa989e349af37c0cd687c8eecd1d26d78a1cc
skyedemoya <hello@skyedemoya.com>
2020-09-18 03:24:55 +0000

    Added get_stored_number() function and changed json file's name.

--- files/number-pie.py
+++ files/number-pie.py
@@ -1,12 +1,25 @@
 import json
 
+def get_stored_number():
+    """Retrieve stored number."""
+    filename = 'store_number.json'
+    try:
+        with open(filename) as f:
+            num = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return num
+
 def get_number():
     """Prompt user for their favorite number and store it in a JSON file."""
     num = input("What's your favorite number? ")
-    filename = 'number.json'
+    filename = 'store_number.json'
     with open(filename, 'w') as f:
         json.dump(num, f)
     return num
 
+print(get_stored_number())
 print(get_number())
+
 
